822bb153ead6f5b5ec0776321dfe4836f1b5497e
thunderschool <43898635+thunderschool@users.noreply.github.com>
2021-02-28 21:55:48 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	cout<<"Este es un mensaje para la consola"<<endl;
+	
+	cin.ignore();
+	return 0;
+}
